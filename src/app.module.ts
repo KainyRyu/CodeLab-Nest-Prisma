a3786baae867820c14de15cbcb74a917dc2c82fe
Kainy Ryu <kainy2109@gmail.com>
2021-05-26 15:42:12 +0900

    The first setting for 1:N relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule, UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
